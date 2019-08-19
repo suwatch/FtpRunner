22bbcb2b2b4199af1db990ce230558039f5e04f3
Suwath Ch <suwatch@microsoft.com>
2019-08-19 10:19:33 -0700

    initial commit

---
+++ Logger.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FtpRunner
+{
+    class Logger
+    {
+        private const int MaxFiles = 5;
+        private const int MaxLines = 500;
+        private int _lines = 0;
+        private string _fileName = null;
+        private static Lazy<int> PID = new Lazy<int>(() => Process.GetCurrentProcess().Id);
+
+        public readonly static Logger Instance = new Logger();
+
+        Logger()
+        {
+            LogPath = Path.Combine(Environment.GetEnvironmentVariable("TEMP"), Path.GetFileNameWithoutExtension(Assembly.GetExecutingAssembly().Location));
+            Directory.CreateDirectory(LogPath);
+        }
+
+        public string LogPath { get; set; }
+
+        public void AppendLine(object obj)
+        {
+            AppendLine("{0}", obj);
+        }
+
+        public void AppendLine(string format, params object[] args)
+        {
+            var strb = new StringBuilder();
+            strb.AppendFormat("{0:s} - ", DateTime.UtcNow);
+            strb.AppendFormat(format, args);
+            lock (typeof(Logger))
+            {
+                var filePath = GetLogFile();
+                File.AppendAllLines(filePath, new[] { strb.ToString() });
+                ++_lines;
+
+                Console.WriteLine(strb);
+            }
+        }
+
+        private string GetLogFile()
+        {
+            if (_fileName == null || _lines > MaxLines)
+            {
+                PurgeOldFile();
+                _fileName = Path.Combine(LogPath, string.Format("Job_{0}_{1}.txt", DateTime.UtcNow.ToString("yyyy-MM-dd_HH-mm-ss"), PID.Value));
+                _lines = 0;
+            }
+
+            return _fileName;
+        }
+
+        private void PurgeOldFile()
+        {
+            var files = Directory.GetFiles(LogPath, "Job_*.txt", SearchOption.TopDirectoryOnly)
+                .OrderBy(f => f)
+                .ToList();
+            var toRemove = files.Count - MaxFiles;
+            if (toRemove > 0)
+            {
+                foreach(var file in files.Take(toRemove))
+                {
+                    File.Delete(file);
+                }
+            }
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,274 @@
+﻿using System;
+using System.Collections.Concurrent;
+using System.IO;
+using System.Net;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace FtpRunner
+{
+    class Program
+    {
+        static bool _verbose = false;
+
+        static void Main(string[] args)
+        {
+            ServicePointManager.ServerCertificateValidationCallback = delegate { return true; };
+
+            try
+            {
+                if (args.Length != 0)
+                {
+                    _verbose = true;
+                    foreach (var arg in args)
+                    {
+                        RunFtpForStamp(arg).Wait();
+                    }
+                    return;
+                }
+
+                _verbose = Environment.GetEnvironmentVariable("FTPRUNNER_VERBOSE") == "1";
+
+                while (true)
+                {
+                    RunFtpRunner().Wait();
+                }
+            }
+            catch (Exception ex)
+            {
+                Logger.Instance.AppendLine(ex);
+            }
+        }
+
+        static ConcurrentDictionary<string, int> _maxStampIndexes = new ConcurrentDictionary<string, int>();
+
+        static string[] _stamps = new[]
+        {
+            "waws-prod-am2-001",
+            "waws-prod-auh-001",
+            "waws-prod-bay-001",
+            "waws-prod-ber-001",
+            "waws-prod-blu-001",
+            "waws-prod-bm1-001",
+            "waws-prod-bn1-001",
+            "waws-prod-brse-001",
+            "waws-prod-cbr20-001",
+            "waws-prod-cbr21-001",
+            "waws-prod-ch1-001",
+            "waws-prod-chw-001",
+            "waws-prod-cpt20-001",
+            "waws-prod-cq1-001",
+            "waws-prod-cw1-001",
+            "waws-prod-cy4-001",
+            "waws-prod-db3-001",
+            "waws-prod-dm1-001",
+            "waws-prod-dxb-001",
+            "waws-prod-euapbn1-001",
+            "waws-prod-euapdm1-001",
+            "waws-prod-euapdm1-505",
+            "waws-prod-fra-001",
+            "waws-prod-hk1-001",
+            "waws-prod-jinc-001",
+            "waws-prod-jinw-001",
+            "waws-prod-jnb21-001",
+            "waws-prod-kw1-001",
+            "waws-prod-ln1-001",
+            "waws-prod-ma1-001",
+            "waws-prod-ml1-001",
+            "waws-prod-mrs-001",
+            "waws-prod-msftbay-901",
+            "waws-prod-msftblu-901",
+            "waws-prod-msftdb3-901",
+            "waws-prod-msfthk1-901",
+            "waws-prod-mwh-001",
+            "waws-prod-os1-001",
+            "waws-prod-osl-001",
+            "waws-prod-par-001",
+            "waws-prod-pn1-001",
+            "waws-prod-ps1-001",
+            "waws-prod-qac-001",
+            "waws-prod-se1-001",
+            "waws-prod-sec-001",
+            "waws-prod-ses-001",
+            "waws-prod-sg1-001",
+            "waws-prod-sn1-001",
+            "waws-prod-svg-001",
+            "waws-prod-sy3-001",
+            "waws-prod-ty1-001",
+            "waws-prod-usw3-001",
+            "waws-prod-xyz-001",
+            "waws-prod-yq1-001",
+            "waws-prod-yt1-001",
+            "waws-prod-zrh-001",
+        };
+
+        static async Task RunFtpRunner()
+        {
+            var semaphore = new SemaphoreSlim(initialCount: 5);
+            foreach (var stamp0 in _stamps)
+            {
+                await RunFtpForStamp0(stamp0, semaphore);
+            }
+        }
+
+        static async Task RunFtpForStamp0(string stamp0, SemaphoreSlim semaphore)
+        {
+            var parts = stamp0.Split('-');
+            var index = int.Parse(parts[3]);
+            int maxIndex;
+            if (!_maxStampIndexes.TryGetValue(stamp0, out maxIndex))
+            {
+                maxIndex = -1;
+            }
+
+            while (true)
+            {
+                var stamp = string.Format("{0}-{1}-{2}-{3:000}", parts[0], parts[1], parts[2], index);
+
+                if (index > maxIndex)
+                {
+                    if (await CheckDns(stamp))
+                    {
+                        maxIndex = index;
+
+                        _maxStampIndexes.AddOrUpdate(stamp0, maxIndex, (_, __) => maxIndex);
+                    }
+                    else if (index > (maxIndex + 4))
+                    {
+                        break;
+                    }
+                }
+
+                var acquired = await semaphore.WaitAsync(TimeSpan.FromMinutes(1));
+                if (!acquired)
+                {
+                    Environment.FailFast($"FtpRunner: timeout waiting for semaphore {stamp}");
+                    return;
+                }
+
+                _ = RunFtpForStamp(stamp, semaphore);
+
+                index = index + 2;
+            }
+        }
+
+        static async Task RunFtpForStamp(string stampName, SemaphoreSlim semaphore)
+        {
+            try
+            {
+                await RunFtpForStamp(stampName);
+            }
+            catch (Exception ex)
+            {
+                Logger.Instance.AppendLine($"{stampName} {ex.Message}");
+            }
+            finally
+            {
+                semaphore.Release();
+            }
+        }
+
+        static async Task RunFtpForStamp(string stampName)
+        {
+            try
+            {
+                var userName = Environment.GetEnvironmentVariable("FTP_USERNAME");
+                var password = Environment.GetEnvironmentVariable("FTP_PASSWORD");
+
+                var siteName = userName.StartsWith("$") ? userName.Substring(1) : stampName.Replace("waws-prod-", "antares-prod-");
+                var ftpHostname = $"{stampName}.ftp.azurewebsites.windows.net";
+                var drips = Dns.GetHostAddresses(ftpHostname);
+
+                foreach (var enableSsl in new[] { false }) // , true })
+                {
+                    await PingFtp($"ftp://{ftpHostname}/site/wwwroot", siteName, userName, password, enableSsl);
+                    foreach (var drip in drips)
+                    {
+                        await PingFtp($"ftp://{drip}/site/wwwroot", siteName, userName, password, enableSsl);
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                Logger.Instance.AppendLine($"{stampName} {ex.Message}");
+            }
+        }
+
+        static async Task PingFtp(string url, string siteName, string userName, string password, bool EnableSsl)
+        {
+            var start = DateTime.UtcNow;
+            var strb = new StringBuilder();
+            strb.Append($"Connecting to {siteName}, {url} - ");
+
+            try
+            {
+                FtpWebRequest request = (FtpWebRequest)WebRequest.Create(url);
+                request.Method = WebRequestMethods.Ftp.ListDirectory;
+                request.Credentials = new NetworkCredential(String.Format("{0}\\{1}", siteName, userName), password);
+                if (EnableSsl)
+                {
+                    // ExplicitSSLFtps
+                    // Explicit do control over 21 and data securedly over data channel
+                    request.EnableSsl = EnableSsl;
+                }
+
+                using (WebResponse response = await request.GetResponseAsync())
+                {
+                    strb.AppendFormat("connected ({0}ms) ", (int)(DateTime.UtcNow - start).TotalMilliseconds);
+                    using (var streamReader = new StreamReader(response.GetResponseStream()))
+                    {
+                        while (!streamReader.EndOfStream)
+                        {
+                            var line = await streamReader.ReadLineAsync();
+                            strb.Append('.');
+                        }
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                strb.AppendLine(ex.Message);
+                if (!_verbose) Console.Write($"{siteName}({url}).");
+            }
+
+            //  Logger.Instance.AppendLine(strb);
+            if (!_verbose) Console.Write(".");
+            else
+            {
+                lock(typeof(Console))
+                {
+                    Console.WriteLine(strb);
+                }
+            }
+        }
+
+        static async Task<bool> CheckDns(string stamp)
+        {
+            try
+            {
+                var entry = await Dns.GetHostEntryAsync(string.Format("{0}.vip.azurewebsites.windows.net", stamp));
+                if (string.IsNullOrEmpty(entry.HostName))
+                {
+                    throw new InvalidOperationException("entry.HostName is null or empty!");
+                }
+                if (!entry.HostName.StartsWith(stamp, StringComparison.OrdinalIgnoreCase))
+                {
+                    throw new InvalidOperationException($"Invalid entry.HostName {entry.HostName}");
+                }
+                if (!entry.HostName.EndsWith(".cloudapp.net", StringComparison.OrdinalIgnoreCase)
+                    && !entry.HostName.EndsWith(".cloudapp.azure.com", StringComparison.OrdinalIgnoreCase))
+                {
+                    throw new InvalidOperationException($"Invalid entry.HostName {entry.HostName}");
+                }
+
+                return true;
+            }
+            catch (Exception ex)
+            {
+                Logger.Instance.AppendLine($"{stamp} {ex.Message}");
+                return false;
+            }
+        }
+    }
+}
